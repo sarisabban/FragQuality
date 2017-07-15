60046c9854918b4b8f95a0f36f9b77b84d05786c
Sari Sabban <sari.sabban@gmail.com>
2017-07-15 14:51:12 +0100

    Add files via upload

---
+++ FragQuality.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+import os , sys
+
+from pyrosetta import *
+from pyrosetta.rosetta.protocols.simple_moves import *
+from pyrosetta.rosetta.core.fragment import *
+init()
+
+PROTEIN = sys.argv[1]
+FRAGMENT = sys.argv[2]
+
+def NEW(pose):
+	frag = open(FRAGMENT , 'r')
+	rmsd = open('temp.dat' , 'w')
+	for line in frag:
+		if line.startswith(' position:'):
+			line = line.split()
+			size = line[1]
+	frag.close()
+	count = 0
+	for x in range (int(size)):
+		count +=1
+		#Get the pose and make a copy of it to apply changes to
+		pose_copy = Pose()
+		pose_copy.assign(pose)
+		#Setup frame list
+		frames = FrameList()
+		#Setup the 9-mer fragment (9-mer is better than 3-mer for this analysis)
+		fragset = ConstantLengthFragSet(9)
+		fragset.read_fragment_file(FRAGMENT)
+		fragset.frames(count , frames)
+		#Setup the MoveMap
+		movemap = MoveMap()
+		movemap.set_bb(True)
+		#Setup and apply the fragment inserting mover
+		for frame in frames:
+			for frag_num in range( 1 , frame.nr_frags() + 1 ):
+				frame.apply(movemap , frag_num , pose_copy)
+				#Measure the RMSD difference between the original pose and the new changed pose (the copy)
+				RMSD = rosetta.core.scoring.CA_rmsd(pose , pose_copy)
+				print(RMSD , '\t' , count)
+				rmsd.write(str(RMSD) + '\t' + str(count) + '\n')
+				#Reset the copy pose to original pose
+				pose_copy.assign(pose)
+	rmsd.close()
+	#Analyse the RMSD file to get the lowest RMSD for each position
+	data = open('RMSDvsPosition.dat' , 'w')
+	lowest = {} 						#Mapping group number -> lowest value found
+	for line in open('temp.dat'):
+		parts = line.split()
+		if len(parts) != 2:				#Only lines with two items on it
+			continue
+		first = float(parts[0])
+		second = int(parts[1])
+		if first == 0: 					#Skip line with 0.0 RMSD (this is an error from the 9-mer fragment file). I don't know why it happens
+			continue
+		if second not in lowest:
+			lowest[second] = first
+		else:
+			if first < lowest[second]:
+				lowest[second] = first
+	for position, rmsd in lowest.items():
+		print(str(rmsd) + '\t' + str(position))
+		data.write(str(position) + '\t' + str(rmsd) + '\n')
+	data.close()
+	gnuplot = open('gnuplot_sets' , 'w')
+	gnuplot.write("""set terminal postscript
+set output './plot.pdf'
+set encoding iso_8859_1
+set term post eps enh color
+set xlabel 'Position'
+set ylabel 'RMSD (\\305)'
+set yrange [0:]
+set xrange [0:]
+set xtics 1
+set xtics rotate 
+set title 'Fragment Quality'
+set key off
+set boxwidth 0.5
+set style fill solid
+plot 'RMSDvsPosition.dat' with boxes
+exit
+""")
+	gnuplot.close()
+	os.system('gnuplot < gnuplot_sets')
+	os.remove('gnuplot_sets')
+	os.remove('temp.dat')
+#----------------------------------------------------------------------
+pose = pose_from_pdb(PROTEIN)
+NEW(pose)
